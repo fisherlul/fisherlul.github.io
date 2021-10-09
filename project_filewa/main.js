1be9f7c3eb2f0ae9d35831255a19ddd5ecbce765
fisherlul <amazinggumball2301@gmail.com>
2021-10-09 22:35:31 +0700

    Update giao diện

---
+++ project_filewa/main.js
@@ -0,0 +1,22 @@
+
+
+const btnHam = document.querySelector('.ham-btn');
+const btnTime = document.querySelector('.time-btn');
+const navBar = document.getElementById('nav-bar');
+
+btnHam.addEventListener('click', function(){
+    if(btnHam.className !== ""){
+        btnHam.style.display  = "none";
+        btnTime.style.display = "block";
+        navBar.classList.add("show-nav");
+    }
+})
+
+btnTime.addEventListener('click', function(){
+    if(btnHam.className !== ""){
+        this.style.display = "none";
+        btnHam.style.display = "block";
+        navBar.classList.remove("show-nav");
+    }
+})
+
