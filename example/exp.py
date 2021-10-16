d77ab66285d18e242ce41a34569dc94b99bc9b1f
fisherlul <amazinggumball2301@gmail.com>
2021-10-16 19:41:25 +0700

    Update

--- example/exp.py
+++ example/exp.py
@@ -1,2 +1,20 @@
-b = 3.14
-c = float(5)   #kỹ thuật ép kiểu, c sẽ có giá trị là 5.0
+# from math import pi
+# a = float(input("Nhập a"))
+# s = (pi*a**2)/2
+# print(f"Diện tích nửa hình tròn là {s:.4f}")
+
+#######################
+
+# from math import sqrt
+
+# h = float(input("Nhập độ cao h: "))
+# g = 9.8
+# v = sqrt(2*g*h)
+# print("Vận tốc khi chạm đất = ", v)
+
+#######################
+
+a = float(input("Nhập chiều dài a: "))
+b = float(input("Nhập chiều rộng b: "))
+s = a*b
+print("Diện tích hình chữ nhật là: ", s)
--- project_filewa/form/form.js
+++ project_filewa/form/form.js
@@ -1,17 +1,76 @@
-// SIGNUP FORM JS
-let formSignup = document.getElementById("formSignup");
+// SIGNIN FORM JS
+let domSignin = document.getElementsByClassName("container--signin");
 
 let users = localStorage.getItem("user")
-let usersList = [] //trống
-if(users){
-  usersList = JSON.parse(localStorage.getItem("user"))
+let usersList = []
+if (users) {
+    usersList = JSON.parse(localStorage.getItem("user"))
 }
 
-formSignup.onsubmit = function (e) { // function báo lỗi
-    e.preventDefault();
+domSignin.onsubmit = function (e) {
+    let email = domSignin.email.value;
+    let password = domSignin.password.value;
+
+    for (let i = 0; i < usersList.length; i++) {
+        if (usersList[i].email == email) {
+            if (usersList[i].password == password) {
+                window.open("./home.html", "_self")
+            } else {
+                console.log("Wrong Password!")
+            }
+            break
+        } else {
+            sweetAlert("error", "email does not exist")
+        }
+    }
+};
+
+function sweetAlert(icon, message) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    
+    Toast.fire({
+        icon: icon,
+        title: message,
+    })
+    
+}
+
+// FORM EFFECTS
+const signInBtn = document.getElementById("signIn");
+        const signUpBtn = document.getElementById("signUp");
+        const fistForm = document.getElementById("form1");
+        const secondForm = document.getElementById("form2");
+        const container = document.querySelector(".container");
+
+        signInBtn.addEventListener("click", () => {
+            container.classList.remove("right-panel-active");
+        });
+
+        signUpBtn.addEventListener("click", () => {
+            container.classList.add("right-panel-active");
+        });
+
+        fistForm.addEventListener("submit", (e) => e.preventDefault());
+        secondForm.addEventListener("submit", (e) => e.preventDefault());
+   
+// SIGNUP FORM 
+let formSignup = document.getElementsByClassName("container--signup");
+
+formSignup.onsubmit = function (e) { 
 
     setTextError("#emailError", "");
-    setTextError("#passwordError", "");  //reset lỗi
+    setTextError("#passwordError", "");  
+    setTextError("#cfPasswordError", "");
 
     let email = formSignup.email.value;
     let password = formSignup.password.value;
