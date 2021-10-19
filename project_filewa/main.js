f49420b17fdd227cb362e71aac083666efada4cf
fisherlul <amazinggumball2301@gmail.com>
2021-10-19 19:56:34 +0700

    Render Data từ API

--- project_filewa/main.js
+++ project_filewa/main.js
@@ -22,13 +22,50 @@ btnTime.addEventListener('click', function(){
 ;
 
 let getDataNews = async ()=>{
-    let draw = await fetch('https://vnexpress.net/microservice/home')
-    let data = await draw.data
+    let draw = await fetch('https://vnexpress.net/microservice/home');
+    let data = await draw.data;
+
+    renderDataNews(mixData(data.data))
+};
+
+// let getDataInfo = async ()=>{
+//     let draw = await fetch('https://vnexpress.net/microservice/home')
+//     let data = await draw.data
+// }
+let mixData = (data)=>{
+    let newData = []
+    let keyData = ["1001002", "1001005", "1001006", "1001007", "1001009", "1001011"]
+    for(let i= 0; i< 10; i++){
+        let topicIndex = keyData[getRandomInt(0, keyData.length)]
+        let newsIndex = getRandomInt(0, 7) 
+
+        newData.push(data[topicIndex].data[newsIndex])
+    }
+
+    return newData
 }
 
-let getDataInfo = async ()=>{
-    let draw = await fetch('https://vnexpress.net/microservice/home')
-    let data = await draw.data
-}
+let renderDataNews = (data)=>{
+    let dom= document.querySelector("#a")
+    dom.innerHTML = "";
+    for(let i=0; i<data.length; i++){
+        let html = `<article>
+        <img src = "images/bottom-left-2.jpg">
+        <div>
+            <h3>iPad Pro, reviewed: Has the iPad become my main computer now?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi iure modi animi cupiditate. Explicabo, nihil?</p>
 
-getData()
+            <a href = "#">Read More <span>>></span></a>
+        </div>
+    </article>`
+
+    dom.innerHTML += html;
+    }
+};
+getDataNews();
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
