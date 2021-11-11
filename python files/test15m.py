b911cb05330d8adfcb951db8e345499a0a46958e
fisherlul <amazinggumball2301@gmail.com>
2021-11-11 16:35:25 +0700

    Bài kiểm tra hệ số 1

--- python files/test15m.py
+++ python files/test15m.py
@@ -1,32 +1,30 @@
-# TÌM SỐ CHÍNH PHƯƠNG
-n = int(input("Hãy nhập n: "))
-check = False
- 
-for i in range(1, n+1):
-    if(i**2 == n):
-       check = True
-       break
-
-if (check == True):
-    print(n, " là số chính phương")
+# BÀI 1
+n = int(input ("Mời bạn nhập số n:"))
+if n<=50:
+    print (1500*n, "là số tiền phải trả")
+elif 50<n<=100:
+    print (1500*50+2000*(n-50), "là số tiền phải trả")
 else:
-    print(n, " không phải là số chính phương")
+    print (1500*50+2000*50+3000*(n-100), "là số tiền phải trả")
 
-
-# TÍNH TIỀN ĐIỆN
-n = int(input("Hãy nhập n: "))
-if n <= 50:
-    print(1500*n);
-elif n>50 and n<= 100:
-    print(1500 * 50 + (n-50) * 2000);
+# BÀI 2
+N = int(input('nhập số nguyên dương chẵn:'))
+s = float()
+if N>0 and N%2==0:
+    for a in range(2, N+2, 2):
+        s += (a+1)/a
+    print('tổng là: ' + str(s))
 else:
-    print(1500 * 50 + 2000 * 50 + (n - 100) * 2500)
+    print('hãy nhập số NGUYÊN DƯƠNG CHẴN')
 
-
-# TÍNH TỔNG CÁC SỐ CHẴN
-n = int(input("Hãy nhập n: "))
-s=2
-
-for i in range (2, n, 2):
-    s+=i
-print (f"gia tri tong la {s:.2f}")
+# BÀI 3
+a=int(input("Nhập a = "))
+s = 1/a+1;
+N = 1;
+if a <= 5 :
+    print (" hãy nhập a lớn hơn 5")
+else:
+  while 1/(a+N)> 0.0005:
+      s=s+1/(a+N);
+      N=N+2
+  print (f"giá trị của tổng là {s:.2f}")
