3987cb50eeaaf797132777885125f87f530bc9d8
fisherlul <amazinggumball2301@gmail.com>
2022-01-13 16:20:42 +0700

    update

--- python files/ktra15p.py
+++ python files/ktra15p.py
@@ -8,4 +8,4 @@ print(B)
 print("Phần tử lớn nhất của dãy là:", max(B))  #câu b
 
 print("Phần tử nhỏ nhất của dãy là:", min(B))  #câu c
-print ("Chỉ số nhỏ nhất của nó là", B.index(min(B)))
+print("Chỉ số nhỏ nhất của nó là", B.index(min(B)))
