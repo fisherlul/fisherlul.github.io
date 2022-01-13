cdf28932f4c8fd52c3ea8edfc74b016cbe366770
fisherlul <87922824+fisherlul@users.noreply.github.com>
2022-01-13 16:03:14 +0700

    Update

--- python files/ktra15p.py
+++ python files/ktra15p.py
@@ -1,12 +1,13 @@
-n=int(input("Nhập số phần tử:"))
+n=int(input("Nhập số phần tử:"))  #câu a
 B=[]
 for i in range (0,n):
     i=int(input("Nhập phần tử x:"))
     B.append(i)
 print(B)
 
-print("Phần tử lớn nhất của dãy là:", max(B))
-print("Phần tử nhỏ nhất của dãy là:", min(B))
+print("Phần tử lớn nhất của dãy là:", max(B))  #câu b
+
+print("Phần tử nhỏ nhất của dãy là:", min(B))  #câu c
 chiso = []
 for i in range (n):
     if B[i] == min (B):
