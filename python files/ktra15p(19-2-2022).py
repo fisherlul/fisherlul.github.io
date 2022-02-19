ec9b35e47d412cdaa00a84c150c8207ae89996b5
fisherlul <amazinggumball2301@gmail.com>
2022-02-19 15:18:00 +0700

    Update

---
+++ python files/ktra15p(19-2-2022).py
@@ -0,0 +1,28 @@
+#BÀI 1
+S1 = input("Nhập xâu thứ nhất: ")
+S2 = input("Nhập xâu thứ hai: ")
+S3 = input("Nhập xâu thứ ba: ")
+count=0
+if S1 == S1[::-1]:
+    count+=1
+else:
+    count+=0
+if S2 == S2[::-1]:
+    count+=1
+else:
+    count+=0
+if S3 == S3[::-1]:
+    count+=1
+else:
+    count+=0
+print(count)
+
+# BÀI 2
+s = str(input('Nhập xâu:'))
+for i in range (0,10):
+    print(f'số {i} xuất hiện {s.count(str(i))} lần')
+
+# BÀI 3
+a = input('Nhập xâu S: ')
+b = a.replace('anh','em')
+print('S=',b)
