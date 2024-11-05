ecf23eab69e0c3891ddf54267b52b4a5380423c1
fisherlul <87922824+fisherlul@users.noreply.github.com>
2024-11-05 16:33:32 +0100

    Delete file

--- .python/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    result.append(left[index_left])
-            index_left += 1
-        else:
-            result.append(right[index_right])
-            index_right += 1
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+for _ in range(positions[i]):
+            print('.', end = '')
+        print('X', end = '')
+        for _ in range(n-positions[i]-1):
+            print('.', end = '')
+        print('\n')
