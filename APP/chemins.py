1106001e1d72f22d0e88c3e0dd3003b6ee81c6c0
fisherlul <87922824+fisherlul@users.noreply.github.com>
2025-01-14 16:53:30 +0100

    Small changes

--- APP/chemins.py
+++ APP/chemins.py
@@ -2,11 +2,11 @@ import csv
 import graphviz as gr
 
 ##### Creer dictionnaires #####
-def dico_interactions(interaction=str):
+def dico_interactions(interaction='favorise') -> dict:
     """
     Renvoie un dictionnaire avec les interactions entre les mots du fichier passé en argument.
     :param interaction: str
-    return: une dictionnaire avec les interactions entre des plantes, dict[int, dict[int, int]]
+    return: une dictionnaire avec les interactions entre des plantes, dict[str, list]
     """
     with open('APP/donnees/data_arcs.csv', 'r', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
@@ -19,7 +19,7 @@ def dico_interactions(interaction=str):
     
     return dico_interactions
 
-def dico_interactions_poids(interaction=str):
+def dico_interactions_poids(interaction='favorise') -> dict:
     """
     Renvoie un dictionnaire avec les interactions entre les mots du fichier passé en argument et les poids des interactions.
     :param interaction: str
@@ -36,7 +36,7 @@ def dico_interactions_poids(interaction=str):
                 dico_interactions[row[0]][row[2]] = int(row[3])
         
     return dico_interactions
-
+print(dico_interactions_poids()['framboisier'])
 ##### Avec auxiliaires #####
 def interaction_especes(jardin: list, dico_interactions: dict) -> dict:
     """
@@ -96,8 +96,8 @@ def trouver_chemin_min_dijkstra(p_init, p_end, adjacents):
         parent = file_attente.pop(0)
         
         if parent in adjacents.keys():
-            for plante, weight in adjacents[parent].items():
-                distance = distances[parent] + weight
+            for plante, poids in adjacents[parent].items():
+                distance = distances[parent] + poids
 
                 if distance < distances.get(plante, float('inf')):
                     distances[plante] = distance
@@ -119,24 +119,26 @@ def dot_graphe(jardin):
     dot = gr.Digraph(f'Graphe du jardin complet')
 
     # Base du jardin
-    for i in range(len(jardin)-1):
-        dot.node(jardin[i])
-        dot.edge(jardin[i], jardin[i+1])
-    
-    # Favorises du jardin
-    edges_drawn = set() # éviter des répétitions des côtés
+    edges_dessines = set() # éviter des répétitions des côtés
 
-    for plante in jardin:
-        if plante in dico_favorise:
-            for p_fav in dico_favorise[plante]:
-                if dico_favorise[plante][p_fav] == min(dico_favorise[plante].values()) and p_fav not in jardin:
-                    if (plante, p_fav) not in edges_drawn:
-                        dot.edge(plante, p_fav, color='blue')
-                        edges_drawn.add((plante, p_fav))
+    if type(jardin) == list:
+        for i in range(len(jardin)-1):
+            dot.node(jardin[i])
+            if (jardin[i], jardin[i+1]) not in edges_dessines:
+                dot.edge(jardin[i], jardin[i+1])
+        
+        # Favorisés du jardin
+        for plante in jardin:
+            if plante in dico_favorise:
+                for p_fav in dico_favorise[plante]:
+                    if dico_favorise[plante][p_fav] == min(dico_favorise[plante].values()) and p_fav not in jardin:
+                        if (plante, p_fav) not in edges_dessines:
+                            dot.edge(plante, p_fav, color='blue')
+                            edges_dessines.add((plante, p_fav))
 
     dot.render('APP/test-output/jardin complet.dot', view=False)
 
-####### Tester ######
+###### Tester ######
 def main(start_vertex, end_vertex):
     # Définir les sommets de départ et de fin
     dico_favorise = dico_interactions_poids("favorise")
@@ -145,16 +147,14 @@ def main(start_vertex, end_vertex):
     chemin_2 = trouver_chemin_min_dijkstra(end_vertex, start_vertex, dico_favorise)
     
     # Vérifier si un chemin existe
-    chemin_min = chemin_1 + chemin_2[1:]
-    if chemin_min == None:
-        print("No path found")
+    if chemin_1 == None or chemin_2 == None:
+        print("Pas de chemin")
     else:
-        print(f"Shortest path from {start_vertex} to {end_vertex}: {chemin_min}")
-
-    return chemin_min
+        print(f"Le chemin le plus court de {start_vertex} à {end_vertex}: {chemin_1 + chemin_2[1:]}")
+        return chemin_1 + chemin_2[1:]
 
 if __name__ == "__main__":
-    main = main('prunier', 'sauge')
+    main = main('cardon', 'menthe')
     dot_graphe(main)
 
 
